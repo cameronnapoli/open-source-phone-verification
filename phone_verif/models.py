4a13d98e3c5b14fe01daed97f11e49f18208c16d
Cameron Napoli <cameronnapoli@gmail.com>
2017-08-25 20:38:27 -0700

    setup for endpoints

--- phone_verif/models.py
+++ phone_verif/models.py
@@ -1,6 +1,10 @@
+"""
+urls.py
+Written by: Cameron Napoli
+"""
+
 from django.db import models
 from django.contrib.auth.models import User
-
 
 class VerificationRequestEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
--- phone_verif/urls.py
+++ phone_verif/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^send_number/', views.index, name='send_number'),
+    url(r'^verify_code/', views.index, name='verify_code'),
 ]
