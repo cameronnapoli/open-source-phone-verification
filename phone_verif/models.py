bcef593b9399f54de725760a80dd7c5e808613c3
Cameron Napoli <cameronnapoli@gmail.com>
2017-09-01 12:14:22 -0700

    Working database match. Added model to extend user class with a new verified attribute. Need to add User auth.

--- phone_verif/models.py
+++ phone_verif/models.py
@@ -1,13 +1,31 @@
 """
-urls.py
+models.py
 Written by: Cameron Napoli
 """
 
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class UserVerified(models.Model):
+    ''' One-to-one table which will hold data on
+        whether a user is verified or not. '''
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE)
+    verified = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'User Verified'
+        verbose_name_plural = 'User Verified'
+
+
 class VerificationRequestEntry(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    ''' DB to hold data for phone number, verification code,
+        and User. '''
+    user = models.ForeignKey(UserVerified,
+                             on_delete=models.SET_NULL,
+                             null=True,
+                             blank=True)
     phone_number = models.CharField(max_length=200)
     verification_code = models.CharField(max_length=200)
 
--- phone_verif/views.py
+++ phone_verif/views.py
@@ -6,7 +6,7 @@ Written by: Cameron Napoli
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
-import json
+from .models import VerificationRequestEntry
 from random import choice
 from twilio.rest import Client
 from . import twilio_config
@@ -14,6 +14,8 @@ from . import twilio_config
 
 # helper functions
 def generate_code(length=4, alphabet='0123456789'):
+    ''' generate verification code.
+        can configure length and characters in code '''
     return ''.join([choice(alphabet) for i in range(length)])
 
 
@@ -36,7 +38,7 @@ def index(request):
 @csrf_exempt
 def send_number(request):
     '''
-    response viewpoint for the initial verify phone request.
+    response endpoint for the initial verify phone request.
 
     client must send phone_number in the format twilio requires
     for example, it needs to look like this for US phone number
@@ -51,6 +53,7 @@ def send_number(request):
 
 
     # TODO: add user auth
+
 
 
     # Initialize Twilio Client and variables
@@ -73,15 +76,55 @@ def send_number(request):
         return json_error_response("SMS error: %s" % e)
 
     # Store verification code
-
+    verif_request_obj = VerificationRequestEntry()
+    # TODO: add user from request auth
+    # verif_request_obj.user
+    verif_request_obj.phone_number = to_number.strip(' \n')
+    verif_request_obj.verification_code = verification_code.strip(' \n')
+    verif_request_obj.save()
 
     print("Verification code stored (phone_number, code) : (%s, %s)" %
-            (to_number, verification_code))
+          (to_number, verification_code))
     return json_success_response()
 
 
 @csrf_exempt
 def verify_code(request):
+    '''
+    response endpoint for the second request once
+    the user receives the verification code through
+    SMS. (must pass phone number in the same format
+    as it was sent in the send_number request)
+
+    POST parameters:
+    phone_number
+    verification_code
+    '''
     if request.method != 'POST':
         return HttpResponse("Request must be POST request.")
-    return HttpResponse("verify_code endpoint.")
+    if 'phone_number' not in request.POST or \
+       'verification_code' not in request.POST:
+        return json_error_response("POST parameter(s) not set.")
+
+
+    # TODO: add user auth
+
+    # query to see if entry in verification codes, exist
+    #   with both phone_number and verification_code
+    query_set = VerificationRequestEntry.objects.filter(
+            phone_number=request.POST['phone_number'].strip(' \n')
+        ).filter(
+            verification_code=request.POST['verification_code'].strip(' \n')
+        )
+
+    if len(query_set) > 0:
+        print("  *** match found (phone_number, verification_code): (%s, %s)" %
+              (request.POST['phone_number'].strip(' \n'),
+               request.POST['verification_code'].strip(' \n')))
+
+        # set user verified equal to true
+
+        return json_success_response()
+    else:
+        return json_error_response('Verification code for provided '
+                                   'phone number not found.')
